c6681a3073a4c51da1979686660b349ff4910877
Abraham Leiro Fernandez <abraham@asoft.es>
2024-09-03 18:42:21 +0200

    chore: Update page title and add menu items from platos.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,141 @@
+import { Icon } from '@iconify/react/dist/iconify.js'
+import logo from './assets/logo.png'
+import { useState } from 'react'
+import platos from './platos.json'
+
 function App() {
 
   return (
     <>
-      <div className="absolute top-0 left-0 bottom-0 right-0 flex items-center justify-center">
-        <div className="text-center">
-          <h1 className="text-2xl font-bold text-gray-800">Aplicación de ejemplo</h1>
-          <p className="text-gray-600">Puedes personalizar esta aplicación en /src/App.jsx</p>
-          <p className="text-xs text-zinc-300">API TOKEN app_{location.hash.replace('#', '')}</p>
+      <div className='dark:bg-zinc-800 min-h-screen dark:text-white'>
+
+        <div>
+          <div className="flex items-center pt-4 justify-center">
+            <img src={logo} className='w-1/2' alt="" />
+          </div>
         </div>
+
+        <div className='mt-6 px-4 space-y-6'>
+          {
+            platos.map((seccion, i) => {
+              return <div key={i}>
+                <p className='text-center text-xl font-semibold '>{seccion.seccion}</p>
+                <div className='mt-4 bg-zinc-100 dark:bg-zinc-700 rounded-xl'>
+                  {
+                    seccion.platos.map((plato, i) => {
+                      return <Plato key={i} descripcion={plato.descripcion} titulo={plato.nombre} precio={plato.precio} alergenos={plato.alergenos} />
+                    })
+                  }
+                </div>
+              </div>
+            })
+          }
+
+        </div>
+
       </div>
     </>
   )
 }
 
+function Plato({ titulo, descripcion, precio, alergenos }) {
+  const [open, setOpen] = useState(false)
+  return <>
+    <div onClick={() => {
+      if (!alergenos) {
+        return
+      }
+      setOpen(!open)
+    }} className='border-b border-zinc-200 dark:border-zinc-500 py-2 px-2 last:border-none'>
+      <div className='flex justify-between'>
+        <p>{titulo}</p>
+        <p>{precio}</p>
+      </div>
+      {
+        open && <div className='mt-2'>
+          <p className='text-sm text-zinc-500'>{descripcion}</p>
+          {
+            alergenos && <>
+              <p className='text-xs text-zinc-400 mt-4 italic mb-2'>Alérgenos</p>
+              <div className='flex gap-4'>
+                {
+                  alergenos?.map((alergeno, i) => <Alergeno key={i} alergeno={alergeno} />)
+                }
+              </div>
+            </>
+          }
+        </div>
+      }
+    </div>
+
+  </>
+}
+
+function Alergeno({ alergeno }) {
+
+  const alergenos = {
+    "huevo": {
+      color: 'text-yellow-500',
+      icon: 'material-symbols:egg',
+      text: "Huevo"
+    },
+    "gluten": {
+      color: 'text-yellow-700',
+      icon: 'mdi:gluten',
+      text: "Gluten"
+    },
+    "lacteos": {
+      color: 'text-blue-400',
+      icon: 'mdi:cow',
+      text: "Lácteos"
+    },
+    "cascara": {
+      color: 'text-green-500',
+      icon: 'mdi:peanut',
+      text: "Trazas de Cáscara"
+    },
+    "crustaceos": {
+      color: 'text-red-500',
+      icon: 'hugeicons:crab',
+      text: "Crustáceos"
+    },
+    "pescado": {
+      color: 'text-blue-500',
+      icon: 'mdi:fish',
+      text: "Pescado"
+    },
+    "sulfitos": {
+      color: 'text-emerald-500',
+      icon: 'mdi:chemical-weapon',
+      text: "Sulfitos"
+    },
+    "mostaza": {
+      color: 'text-yellow-400',
+      icon: 'mdi:soy-sauce',
+      text: "Mostaza"
+    },
+    "soja": {
+      color: 'text-orange-600',
+      icon: 'mdi:soy-sauce',
+      text: "Soja"
+    },
+  }
+
+
+  return <>
+
+    <div className=' flex  items-center justify-center '>
+      <div>
+        <div className='flex items-center justify-center'>
+          <div className='flex items-center w-10 justify-center bg-zinc-200 dark:bg-zinc-600 aspect-square rounded-full'>
+            <Icon icon={alergenos[alergeno].icon} className={alergenos[alergeno].color + ' text-xl'} />
+          </div>
+        </div>
+        <p className={alergenos[alergeno].color + ' text-xs text-center'}>{alergenos[alergeno].text}</p>
+      </div>
+    </div>
+
+  </>
+}
 
 export default App
--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 import { loadEnv } from 'vite'
-import UploadToSFTP from 'vite-plugin-sftp-deploy'
 
-const env = loadEnv('', process.cwd(), '')
-
+const hash = Math.floor(Math.random() * 90000) + 10000;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate', manifest: {
-        name: 'ASoft Default APP',
-        short_name: 'AsoftDevelopment',
-        description: 'ASoft Default APP Template',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
+    react()
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name]` + hash + `.js`,
+        chunkFileNames: `[name]` + hash + `.js`,
+        assetFileNames: `[name]` + hash + `.[ext]`
       }
-    }),
-    UploadToSFTP({
-      host: env.SSH_HOST,
-      username: env.SSH_USER,
-      password: env.SSH_PASSWORD,
-      remotePath: env.SSH_PATH,
-      verbose: true
-    })],
+    }
+  }
 })
